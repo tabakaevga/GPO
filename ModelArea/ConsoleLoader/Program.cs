113cd474612fc647c73d98fd2aba079bcf736c4b
tabakaevga <tabakaevga@gmail.com>
2017-02-28 23:08:23 +0700

    Работа с консолью

--- ModelArea/ConsoleLoader/Program.cs
+++ ModelArea/ConsoleLoader/Program.cs
@@ -9,6 +9,18 @@ namespace ConsoleLoader
 {
     class Program
     {
+        static void Welcoming()
+        {
+            Console.WriteLine("Введите цифру для выбора функции:");
+            Console.WriteLine("1) Вычисление площади окружности");
+            Console.WriteLine("2) Вычисление площади прямоугольника");
+            Console.WriteLine("3) Вычисление площади треугольника");
+            Console.WriteLine("4) Вычисление длины окружности");
+            Console.WriteLine("5) Вычисление длины прямоугольника");
+            Console.WriteLine("6) Вычисление длины треугольника");
+            Console.WriteLine("0) Выход из меню");
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Демонстрация нахождения площадей.");
@@ -21,6 +33,44 @@ namespace ConsoleLoader
             Console.WriteLine("Площадь треугольника со сторонами А=2, B=3, C=4.2");
             Triangle triangle = new Triangle(2, 3, 4.2);
             Console.WriteLine("Площадь треугольника = {0}", triangle.GetArea);
+
+            bool breakingEverything = false;
+            while (true)
+            {
+                Welcoming();
+                int option = Convert.ToInt32(Console.ReadLine());
+                if (breakingEverything)
+                {
+                    break;
+                }
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Введите радиус");
+                        double radius = Convert.ToInt32(Console.ReadLine());
+                        circle.Radius = radius;
+                        Console.WriteLine("Площадь окружности = {0}", circle.GetArea);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 0:
+                        breakingEverything = true;
+                        break;
+                }
+                if (breakingEverything)
+                {
+                    break;
+                }
+
+            }
             Console.Read();
         }
     }
