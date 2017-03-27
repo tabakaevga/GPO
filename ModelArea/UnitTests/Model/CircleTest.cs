918499abab8c2ca40e608c4c0eb1394b8743fcfa
tabakaevga <tabakaevga@gmail.com>
2017-03-28 01:59:23 +0700

    Написал юнит-тесты для класса Circle

--- ModelArea/UnitTests/Model/CircleTest.cs
+++ ModelArea/UnitTests/Model/CircleTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ModelArea;
 using NUnit.Framework;
 
+
 namespace UnitTests.Model
 {
     /// <summary>
@@ -15,13 +16,27 @@ namespace UnitTests.Model
     class CircleTest
     {
         [Test]
-        [TestCase(5, TestName = "Тестирование Area при радиусе = 5.")]
-        [TestCase(0, TestName = "Тестирование Area при радиусе = 0.")]
+        [TestCase(5,  TestName = "Тестирование Area при радиусе = 5.")]
+        [TestCase(-1, ExpectedException = typeof(ArgumentOutOfRangeException), 
+            TestName = "Тестирование Area при радиусе = -1.")]
+        [TestCase("asdf", ExpectedException = typeof(ArgumentException), 
+            TestName = "Тестирование Area при радиусе не выраженном вещественным числом.")]
         public void AreaTest(double radius)
         {
             var circle = new Circle(radius);
             Assert.AreEqual(Math.PI * Math.Pow(radius, 2), circle.Area);
             
         }
+        [Test]
+        [TestCase(5, TestName = "Тестирование Length при радиусе = 5.")]
+        [TestCase(-1, ExpectedException = typeof(ArgumentOutOfRangeException), TestName = "Тестирование Length при радиусе = -1.")]
+        public void LengthTest(double radius)
+        {
+            var circle = new Circle(radius);
+            Assert.AreEqual(Math.PI * 2 * radius, circle.Length);
+
+        }
+
+
     }
 }
