f1bb3cf168c61cbde7b0ca753e17b8628642d303
tabakaevga <tabakaevga@gmail.com>
2017-03-28 01:37:59 +0700

    Начал писать юнит-тесты

---
+++ ModelArea/UnitTests/Model/CircleTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelArea;
+using NUnit.Framework;
+
+namespace UnitTests.Model
+{
+    /// <summary>
+    /// Набор тестов для класса Circle
+    /// </summary>
+    [TestFixture]
+    class CircleTest
+    {
+        [Test]
+        [TestCase(5, TestName = "Тестирование Area при радиусе = 5.")]
+        [TestCase(0, TestName = "Тестирование Area при радиусе = 0.")]
+        public void AreaTest(double radius)
+        {
+            var circle = new Circle(radius);
+            Assert.AreEqual(Math.PI * Math.Pow(radius, 2), circle.Area);
+            
+        }
+    }
+}
