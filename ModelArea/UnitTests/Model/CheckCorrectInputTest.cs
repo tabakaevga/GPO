e63dfa9cc75fe790c60d93406f3a013691910162
tabakaevga <tabakaevga@gmail.com>
2017-03-29 13:14:13 +0700

    Тест класса CheckCorrectInput

---
+++ ModelArea/UnitTests/Model/CheckCorrectInputTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelArea.Tools;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace UnitTests.Model
+{
+    /// <summary>
+    /// Набор тестов класса CheckCorrectInput
+    /// </summary>
+    class CheckCorrectInputTest
+    {
+        [Test]
+        [TestCase(-4, ExpectedException = typeof(ArgumentOutOfRangeException), 
+            TestName = "Проверка корректности ввода отрицательного числа -4")]
+        [TestCase(5, TestName = "Проверка ввода корректного числа 5")]
+        public void CheckDoubleTest(double input)
+        {
+            CheckCorrectInput.CheckDouble(input);
+        }
+    }
+}
