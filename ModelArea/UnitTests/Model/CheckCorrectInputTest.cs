e459049b3aa38753beb0bcafdba7a3c88eaae961
tabakaevga <tabakaevga@gmail.com>
2017-03-31 00:46:59 +0700

    Тесты класса Circle и CheckCorrectInput

--- ModelArea/UnitTests/Model/CheckCorrectInputTest.cs
+++ ModelArea/UnitTests/Model/CheckCorrectInputTest.cs
@@ -18,6 +18,14 @@ namespace UnitTests.Model
         [TestCase(-4, ExpectedException = typeof(ArgumentOutOfRangeException), 
             TestName = "Проверка корректности ввода отрицательного числа -4")]
         [TestCase(5, TestName = "Проверка ввода корректного числа 5")]
+        [TestCase(double.NaN, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Проверка ввода не являющегося числом")]
+        [TestCase(double.PositiveInfinity, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Проверка ввода положительной бесконечностью")]
+        [TestCase(double.PositiveInfinity, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Проверка ввода отрицательной бесконечностью")]
+        [TestCase("asdf", ExpectedException = typeof(ArgumentException),
+            TestName = "Тестирование Area при радиусе не выраженном вещественным числом.")]
         public void CheckDoubleTest(double input)
         {
             CheckCorrectInput.CheckDouble(input);
--- ModelArea/UnitTests/Model/CircleTest.cs
+++ ModelArea/UnitTests/Model/CircleTest.cs
@@ -21,15 +21,30 @@ namespace UnitTests.Model
             TestName = "Тестирование Area при радиусе = -1.")]
         [TestCase("asdf", ExpectedException = typeof(ArgumentException), 
             TestName = "Тестирование Area при радиусе не выраженном вещественным числом.")]
+        [TestCase(double.NaN, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Тестирование Area при радиусе не являющимся числом")]
+        [TestCase(double.PositiveInfinity, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Тестирование Area при радиусе, явлюящимся положительной бесконечностью")]
+        [TestCase(double.PositiveInfinity, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Тестирование Area при радиусе, явлюящимся отрицательной бесконечностью")]
         public void AreaTest(double radius)
         {
             var circle = new Circle(radius);
             Assert.AreEqual(Math.PI * Math.Pow(radius, 2), circle.Area);
-            
         }
+
         [Test]
         [TestCase(5, TestName = "Тестирование Length при радиусе = 5.")]
-        [TestCase(-1, ExpectedException = typeof(ArgumentOutOfRangeException), TestName = "Тестирование Length при радиусе = -1.")]
+        [TestCase(-1, ExpectedException = typeof(ArgumentOutOfRangeException),
+            TestName = "Тестирование Length при радиусе = -1.")]
+        [TestCase("asdf", ExpectedException = typeof(ArgumentException),
+            TestName = "Тестирование Length при радиусе не выраженном вещественным числом.")]
+        [TestCase(double.NaN, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Тестирование Length при радиусе не являющимся числом")]
+        [TestCase(double.PositiveInfinity, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Тестирование Length при радиусе, явлюящимся положительной бесконечностью")]
+        [TestCase(double.PositiveInfinity, ExpectedException = typeof(NotFiniteNumberException),
+            TestName = "Тестирование Area при радиусе, явлюящимся отрицательной бесконечностью")]
         public void LengthTest(double radius)
         {
             var circle = new Circle(radius);
